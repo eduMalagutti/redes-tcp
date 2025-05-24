a6798bf1f8176290753a43db096b00657cf4b506
eduMalagutti <eduardosm2004@gmail.com>
2025-05-24 19:48:59 -0300

    feat: passo 1

--- tcp.py
+++ tcp.py
@@ -1,6 +1,7 @@
 import asyncio
 from tcputils import *
-
+import random
+import os
 
 class Servidor:
     def __init__(self, rede, porta):
@@ -18,8 +19,7 @@ class Servidor:
         self.callback = callback
 
     def _rdt_rcv(self, src_addr, dst_addr, segment):
-        src_port, dst_port, seq_no, ack_no, \
-            flags, window_size, checksum, urg_ptr = read_header(segment)
+        src_port, dst_port, seq_no, ack_no, flags, window_size, checksum, urg_ptr = read_header(segment)
 
         if dst_port != self.porta:
             # Ignora segmentos que não são destinados à porta do nosso servidor
@@ -31,12 +31,10 @@ class Servidor:
         payload = segment[4*(flags>>12):]
         id_conexao = (src_addr, src_port, dst_addr, dst_port)
 
+        # A flag SYN estar setada significa que é um cliente tentando estabelecer uma conexão nova
         if (flags & FLAGS_SYN) == FLAGS_SYN:
-            # A flag SYN estar setada significa que é um cliente tentando estabelecer uma conexão nova
-            # TODO: talvez você precise passar mais coisas para o construtor de conexão
-            conexao = self.conexoes[id_conexao] = Conexao(self, id_conexao)
-            # TODO: você precisa fazer o handshake aceitando a conexão. Escolha se você acha melhor
-            # fazer aqui mesmo ou dentro da classe Conexao.
+            conexao = self.conexoes[id_conexao] = Conexao(self, id_conexao, seq_no)
+            
             if self.callback:
                 self.callback(conexao)
         elif id_conexao in self.conexoes:
@@ -48,12 +46,30 @@ class Servidor:
 
 
 class Conexao:
-    def __init__(self, servidor, id_conexao):
+    def __init__(self, servidor, id_conexao, client_seq_no):
         self.servidor = servidor
         self.id_conexao = id_conexao
         self.callback = None
         self.timer = asyncio.get_event_loop().call_later(1, self._exemplo_timer)  # um timer pode ser criado assim; esta linha é só um exemplo e pode ser removida
         #self.timer.cancel()   # é possível cancelar o timer chamando esse método; esta linha é só um exemplo e pode ser removida
+        self.server_seq_no = int.from_bytes(os.urandom(2), 'big')
+        self.client_seq_no = client_seq_no
+        self.ack_no = self.client_seq_no + 1   
+
+        # Envia o SYN+ACK para o cliente, aceitando a conexão
+        src_addr, src_port, dst_addr, dst_port = self.id_conexao
+        
+        flags = FLAGS_SYN | FLAGS_ACK
+        
+        header = make_header(
+            src_port=dst_port, # porta do servidor
+            dst_port=src_port, # porta do cliente
+            seq_no=self.server_seq_no,
+            ack_no=self.ack_no,
+            flags=flags,
+        )
+        header = fix_checksum(header, dst_addr, src_addr) 
+        self.servidor.rede.enviar(header, src_addr)
 
     def _exemplo_timer(self):
         # Esta função é só um exemplo e pode ser removida
@@ -63,6 +79,7 @@ class Conexao:
         # TODO: trate aqui o recebimento de segmentos provenientes da camada de rede.
         # Chame self.callback(self, dados) para passar dados para a camada de aplicação após
         # garantir que eles não sejam duplicados e que tenham sido recebidos em ordem.
+        
         print('recebido payload: %r' % payload)
 
     # Os métodos abaixo fazem parte da API
@@ -81,6 +98,7 @@ class Conexao:
         # TODO: implemente aqui o envio de dados.
         # Chame self.servidor.rede.enviar(segmento, dest_addr) para enviar o segmento
         # que você construir para a camada de rede.
+        
         pass
 
     def fechar(self):
@@ -88,4 +106,5 @@ class Conexao:
         Usado pela camada de aplicação para fechar a conexão
         """
         # TODO: implemente aqui o fechamento de conexão
+        
         pass
